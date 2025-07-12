6f7ee3f0dec208abbd5341bd7337e68cf9b05e38
tradelab <197946928+Tradelab2025@users.noreply.github.com>
2025-07-12 15:08:22 +0000

    committed

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Auth::routes();
+
 // Include separate route files
 require __DIR__.'/admin.php';
 require __DIR__.'/b2b.php';
 require __DIR__.'/b2c.php';
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
